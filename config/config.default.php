bf65b88473d52db93d80a450cede44d7f2ca4229
Elia Boccini <80979014+Elia-bocc@users.noreply.github.com>
2025-11-23 17:11:46 +0000

    Add files via upload

---
+++ config/config.default.php
@@ -0,0 +1,11 @@
+<?php
+// This file is committed to Git and acts as an example.
+// Copy this file to config.local.php and edit your local settings.
+
+return [
+    'host'     => 'localhost',
+    'port'     => '5432',
+    'dbname'   => 'cdsg_prj',
+    'user'     => 'postgres',
+    'password' => '',
+];
---
+++ config/config.local.php
@@ -0,0 +1,10 @@
+<?php
+// Your personal DB credentials (not committed to Git)
+
+return [
+    'host'     => 'localhost',
+    'port'     => '5432',
+    'dbname'   => 'cdsg_prj',
+    'user'     => 'postgres',
+    'password' => '3366'
+];
